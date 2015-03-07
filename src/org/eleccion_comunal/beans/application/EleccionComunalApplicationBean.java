b7ce0cfc3fd0128b5b6be171af86a9708619d4c0
roberthebt <roberthebt@gmail.com>
2015-03-07 15:19:59 +0000

    Se suben cambios de estilos en dialogo editarVivienda y nuevo controlador de aplicacion.

---
+++ src/org/eleccion_comunal/beans/application/EleccionComunalApplicationBean.java
@@ -0,0 +1,55 @@
+package org.eleccion_comunal.beans.application;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+
+import org.eleccion_comunal.model.dao.ConsejoComunalDAO;
+import org.eleccion_comunal.model.dto.ConsejoComunal;
+
+@ManagedBean(name = "eleccionComunalApplicationBean")
+@ApplicationScoped
+public class EleccionComunalApplicationBean implements Serializable {
+
+	private List<ConsejoComunal> listaConsejosComunales;
+	private ConsejoComunal consejoComunal;
+
+	public EleccionComunalApplicationBean() {
+		super();
+	}
+
+	@PostConstruct
+	public void init() {
+		this.getListaConsejosComunales().addAll(
+				ConsejoComunalDAO.getInstancia().buscarTodasEntidades());
+		this.setConsejoComunal(this.getListaConsejosComunales().get(0));
+	}
+
+	public List<ConsejoComunal> getListaConsejosComunales() {
+		if (listaConsejosComunales == null) {
+			listaConsejosComunales = new ArrayList<ConsejoComunal>();
+		}
+		return listaConsejosComunales;
+	}
+
+	public void setListaConsejosComunales(
+			List<ConsejoComunal> listaConsejosComunales) {
+		this.listaConsejosComunales = listaConsejosComunales;
+	}
+
+	public ConsejoComunal getConsejoComunal() {
+		if (consejoComunal == null) {
+			consejoComunal = new ConsejoComunal();
+		}
+		return consejoComunal;
+	}
+
+	public void setConsejoComunal(ConsejoComunal consejoComunal) {
+		this.consejoComunal = consejoComunal;
+	}
+
+}
